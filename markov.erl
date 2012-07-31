761e94335b38a28154f97697c2b5a21633824901
Amy <oxling@gmail.com>
2012-07-31 15:13:49 -0400

    Added ability to emit using input text; comments
    If you use write_line with input text, the output
    will be based on keywords found in the input text.

--- markov.erl
+++ markov.erl
@@ -3,7 +3,7 @@
 
 -record(chain, {words, next=[]}).
 
-% Generate a forward and backward chain from a corpus of training data
+% Generate forward and backward chains from a corpus of training data
 parse_file(File_Name) when is_list(File_Name)->
 	{ok, File} = file:open(File_Name, [read]),
 	Lines = append_line(File, []),
@@ -15,6 +15,29 @@ parse_file(File_Name) when is_list(File_Name)->
 write_line({Forward_Data, Backward_Data}) ->
 	Forward = emit(forward, Forward_Data, []),
 	Output = emit(backward, Backward_Data, lists:reverse(Forward)),
+	print_output(Output).
+
+% Output a line based on some input data.
+write_line({Forward_Data, Backward_Data}, Input) ->
+	% Find all the interesting keywords in the input
+	Keys = lists:filter(fun(S) -> (is_word(S)) and (is_interesting(S)) end, symbols(Input, [])),
+
+	% Find all grams that have their first word matching a key
+	Grams = [Gram || {chain, Gram, _} <- Forward_Data],
+	Key_Grams = [X || X <- lists:map(fun(X) -> lists:keyfind(X, 1, Grams) end, Keys),
+												 X /= false],
+	
+	case Key_Grams of
+		[] -> write_line({Forward_Data, Backward_Data}); % No matches, random output
+		_ -> 
+			% Generate output based on a random matching gram
+			{W1, W2, W3} = lists:nth(random:uniform(length(Key_Grams)), Key_Grams),
+			Forward = emit(forward, Forward_Data, [W1, W2, W3]),
+			Output = emit(backward, Backward_Data, lists:reverse(Forward)),
+			print_output(Output)
+	end.
+
+print_output(Output) ->
 	io:format("~s\n", [string:join(lists:map(fun(X) -> atom_to_list(X) end, Output), "")]).
 
 is_alphanumeric(Char) ->
@@ -25,9 +48,30 @@ is_alphanumeric(Char) ->
 		_ -> true
 	end.
 
+is_word(Sym) when is_atom(Sym) ->
+	is_word(atom_to_list(Sym));
+is_word(Sym) when is_list(Sym) ->
+	lists:foldl(fun(C, true) -> is_alphanumeric(C); (_C, false) -> false end, true, Sym).
+
+is_interesting(Sym) when is_list(Sym) ->
+	is_interesting(list_to_atom(Sym));
+is_interesting(Sym) when is_atom(Sym) ->
+	case Sym of
+		'IN' -> false;
+		'ON' -> false;
+		'OF' -> false;
+		'THE' -> false;
+		'IS' -> false;
+		'ARE' -> false;
+		'AND' -> false;
+		_ -> true
+	end.
+
+
 atomize(String) ->
 	list_to_atom(string:to_upper(String)).
 
+% Turns a line of text into atomized symbols
 symbols(Line, Symbols)  when length(Line) > 0 ->
 	Found = lists:takewhile(fun(C) -> markov:is_alphanumeric(C) end, Line),
 	case Found of 
@@ -79,6 +123,7 @@ emit(forward, Data, Output) when length(Output) > 2 ->
 emit(forward, _Data, Output) ->
 	Output.
 
+% Update the markov chain, using an array of lines
 train([Line | Rest], Data, forward) ->
 	train(Rest, train_line(Line, Data), forward);
 train(Lines, Data, backward) ->
@@ -86,6 +131,7 @@ train(Lines, Data, backward) ->
 train([], Data, _direction) ->
 	Data.
 
+% Update the markov chain with a line of text.
 train_line(Line, Data) when length(Line) > 3 ->
 	[W1, W2, W3, W4 | Rest] = Line,
 	Chain = lists:keyfind({W1, W2, W3}, #chain.words, Data),
